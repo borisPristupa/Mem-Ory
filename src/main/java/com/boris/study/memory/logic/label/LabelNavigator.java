a0ddb9f0e2ecb9a4a8e56fb1719242ec9551b03b
borisPristupa <boris.pristupa@gmail.com>
2019-02-17 16:06:42 +0300

    +dynamically updating data list

--- src/main/java/com/boris/study/memory/logic/label/LabelNavigator.java
+++ src/main/java/com/boris/study/memory/logic/label/LabelNavigator.java
@@ -1,6 +1,7 @@
 package com.boris.study.memory.logic.label;
 
 import com.boris.study.memory.data.entity.Client;
+import com.boris.study.memory.data.entity.Data;
 import com.boris.study.memory.data.entity.Label;
 import com.boris.study.memory.data.entity.ScenarioState;
 import com.boris.study.memory.data.repository.DataRepository;
@@ -38,118 +39,118 @@ public class LabelNavigator extends BotScenario {
 
         Client client = getClient();
 
+        int stageInitialized = 0;
+
+        if (null == getStage() || getStage() < stageInitialized) {
+            logger.info("LabelNavigator - Starting for " + client);
+            init(request);
+            setStage(stageInitialized);
+            return false;
+        }
+
+        logger.info("LabelNavigator - Continuing for " + client);
         if (request.update.hasCallbackQuery()) {
-            Message oldContentsMessage = request.update.getCallbackQuery().getMessage();
-            String callback = request.update.getCallbackQuery().getData();
-
-            Label callbackLabel = labelRepository.findByNameAndClientId(callback, client.getId()
-            ).orElseThrow(() -> new IllegalStateException("Label '" + callback + "' does not exist"));
-
-            if (getState().has(callback) || isLabelInPath(callback)) {
-                if (getState().has(callback)) {
-                    getState().remove(callback);
-                    setState(getState());
-                }
-
-                EditMessageText editMessageText = botUtils.plainEdit(
-                        oldContentsMessage,
-                        "Here is a list of data, marked by '" + callback + "'" +
-                                oldContentsMessage.getText().substring(oldContentsMessage.getText().indexOf("\n"))
-                ).setReplyMarkup(sublabelsMarkup(callbackLabel));
-
-                try {
-                    bot.execute(editMessageText);
-                } catch (TelegramApiRequestException e) {
-                    e.printStackTrace();
-                    System.err.println(e.getApiResponse());
-                } catch (TelegramApiException e) {
-                    logger.error("Failed to check sublabels of label info in request " + request, e);
-                }
-            } else {
-                Label parent = labelRepository.findByNameAndClientId(
-                        getCurrentDir(), client.getId()
-                ).orElseThrow(() -> new IllegalStateException("Label '" + getCurrentDir() + "' does not exist"));
-
-                setState(getState().put(callback, "checked"));
-                try {
-                    bot.execute(new EditMessageReplyMarkup()
-                            .setChatId(oldContentsMessage.getChatId())
-                            .setMessageId(oldContentsMessage.getMessageId())
-                            .setReplyMarkup(sublabelsMarkup(parent)));
-                } catch (TelegramApiException e) {
-                    logger.error("Failed to check label info in request " + request, e);
-                }
-            }
+            handleCallback(request);
             return false;
         }
 
-        logger.info("LabelNavigator - Starting for " + client);
-
-        try {
-            Optional<Label> allDataOp = labelRepository.findByNameAndClientId("all data", client.getId());
-            Label allData = allDataOp.orElseThrow(() -> new IllegalStateException(String.format(
-                    "No 'all data' label found for user %s, request %s",
-                    client, request)));
-
-            SendMessage sendLabelInfo = botUtils.markdownMessage(
-                    "Search for labels and data. Send me text (without line breaks and its length should " +
-                            "be less than 50 characters) to save it as a label. Send me a data URL to assign " +
-                            "your current label to it",
-                    botUtils.retrieveChat(request.update).getId()
-            );
-            sendLabelInfo.setReplyMarkup(new ReplyKeyboardMarkup().setKeyboard(
-                    Arrays.asList(
-                            new KeyboardRow() {{
-                                add(new KeyboardButton("Send data by selected label(s)"));
-                            }},
-                            new KeyboardRow() {{
-                                add(new KeyboardButton("Rename label"));
-                            }},
-                            new KeyboardRow() {{
-                                add(new KeyboardButton("Delete label"));
-                            }},
-                            new KeyboardRow() {{
-                                add(new KeyboardButton("To main menu"));
-                            }}
-                    )).setResizeKeyboard(true)
-            );
-            bot.execute(sendLabelInfo);
-//        ---------------------------------------------------------------------------
-
-            StringBuilder dataContent =
-                    new StringBuilder("Here is a list of data, marked by '" + allData.getName() + "'\n");
-
-            Set<String> labelNamesRecursive = allData.getAllSonsRecursively().stream()
-                    .map(Label::getName)
-                    .collect(Collectors.toSet());
-
-            int lengthOld = dataContent.length();
-            dataRepository.findAllByLabelNamesAndClientId(labelNamesRecursive, client.getId()).forEach(
-                    data -> dataContent.append("\n")
-                            .append(data.getUrl()).append(" - ").append(data.getDescription()));
-            if (dataContent.length() == lengthOld) {
-                dataContent.append("\n").append("Well, no data yet");
-            }
-
-            SendMessage sendLabelContents = botUtils.plainMessage(
-                    dataContent.toString(), botUtils.retrieveChat(request.update).getId()
-            );
-
-            setState(getState().put("PATH", ""));
-            sendLabelContents.setReplyMarkup(sublabelsMarkup(allData));
-            bot.execute(sendLabelContents);
-
-            return false;
-        } catch (Exception e) {
-            logger.error("Failed to process LabelNavigator in update " + request.update, e);
-        }
 
         setStage(null);
         logger.info("LabelNavigator - Finished for " + client);
         return true;
     }
 
-    private InlineKeyboardMarkup sublabelsMarkup(Label label) {
+    private void init(Request request) {
+        Client client = getClient();
+
+        Optional<Label> allDataOp = labelRepository.findByNameAndClientId("all data", client.getId());
+        Label allData = allDataOp.orElseThrow(() -> new IllegalStateException(String.format(
+                "No 'all data' label found for user %s, request %s",
+                client, request)));
+        setState(getState().put("PATH", ""));
+
+        SendMessage sendLabelInfo = botUtils.markdownMessage(
+                "Search for labels and data. Send me text (without line breaks and its length should " +
+                        "be less than 50 characters) to save it as a label. Send me a data URL to assign " +
+                        "your current label to it",
+                botUtils.retrieveChat(request.update).getId()
+        );
+        sendLabelInfo.setReplyMarkup(new ReplyKeyboardMarkup().setKeyboard(
+                Arrays.asList(
+                        new KeyboardRow() {{
+                            add(new KeyboardButton("Send data by selected label(s)"));
+                        }},
+                        new KeyboardRow() {{
+                            add(new KeyboardButton("Rename label"));
+                        }},
+                        new KeyboardRow() {{
+                            add(new KeyboardButton("Delete label"));
+                        }},
+                        new KeyboardRow() {{
+                            add(new KeyboardButton("To main menu"));
+                        }}
+                )).setResizeKeyboard(true)
+        );
+
+        SendMessage sendLabelContents = botUtils.plainMessage(
+                createDataList(allData), botUtils.retrieveChat(request.update).getId()
+        );
+        sendLabelContents.setReplyMarkup(createSublabelsMarkup(allData));
+
+        try {
+            bot.execute(sendLabelInfo);
+            bot.execute(sendLabelContents);
+        } catch (TelegramApiException e) {
+            logger.error("Failed to initialize LabelNavigator and send dat aof 'all data' in request " + request, e);
+        }
+    }
+
+    private void handleCallback(Request request) {
+        Client client = getClient();
+
+        Message oldContentsMessage = request.update.getCallbackQuery().getMessage();
+        String callback = request.update.getCallbackQuery().getData();
+
+        Label callbackLabel = labelRepository.findByNameAndClientId(callback, client.getId()
+        ).orElseThrow(() -> new IllegalStateException("Label '" + callback + "' does not exist"));
+
+        if (getState().has(callback) || isLabelInPath(callback)) {
+            if (getState().has(callback)) {
+                getState().remove(callback);
+                setState(getState());
+            }
+
+            EditMessageText editMessageText = botUtils.plainEdit(
+                    oldContentsMessage,
+                    createDataList(callbackLabel)
+            ).setReplyMarkup(createSublabelsMarkup(callbackLabel));
+
+            try {
+                bot.execute(editMessageText);
+            } catch (TelegramApiRequestException e) {
+                e.printStackTrace();
+                System.err.println(e.getApiResponse());
+            } catch (TelegramApiException e) {
+                logger.error("Failed to check sublabels of label info in request " + request, e);
+            }
+        } else {
+            Label parent = labelRepository.findByNameAndClientId(
+                    getCurrentDir(), client.getId()
+            ).orElseThrow(() -> new IllegalStateException("Label '" + getCurrentDir() + "' does not exist"));
+
+            setState(getState().put(callback, "checked"));
+            try {
+                bot.execute(new EditMessageReplyMarkup()
+                        .setChatId(oldContentsMessage.getChatId())
+                        .setMessageId(oldContentsMessage.getMessageId())
+                        .setReplyMarkup(createSublabelsMarkup(parent)));
+            } catch (TelegramApiException e) {
+                logger.error("Failed to check label info in request " + request, e);
+            }
+        }
+    }
+
+    private InlineKeyboardMarkup createSublabelsMarkup(Label label) {
         String currentPath = getState().get("PATH").toString();
 
         if (isLabelInPath(label.getName())) {
@@ -159,7 +160,7 @@ public class LabelNavigator extends BotScenario {
             setState(getState().put("PATH", currentPath + "/" + label.getName()));
         }
 
-     // TODO: 16.02.19 Capitalize reserved names, restrict using / in names, make users' labels lowerCased
+        // TODO: 16.02.19 Capitalize reserved names, restrict using / in names, make users' labels lowerCased
 
         List<List<InlineKeyboardButton>> buttons = new ArrayList<>();
         if (null != label.getParents() && !label.getParents().isEmpty()) {
@@ -184,6 +185,35 @@ public class LabelNavigator extends BotScenario {
                 buttons.add(row);
         }
         return new InlineKeyboardMarkup().setKeyboard(buttons);
+    }
+
+    private String createDataList(Label label) {
+        StringBuilder dataContent =
+                new StringBuilder("Here is a list of data, marked by '" + label.getName() + "'\n")
+                        .append("-------------------------------------");
+        int lengthWithoutData = dataContent.length();
+
+        Set<String> labelNamesRecursive = label.getAllSonsRecursively().stream()
+                .map(Label::getName)
+                .collect(Collectors.toSet());
+
+        Set<Data> labeledData =
+                dataRepository.findAllByLabelNamesAndClientId(labelNamesRecursive, label.getClient().getId());
+
+        int i = 1;
+        for (Iterator<Data> it = labeledData.iterator(); it.hasNext(); i++) {
+            Data data = it.next();
+            dataContent.append("\n")
+                    .append(i).append(". ") // number of data in the list
+                    .append(data.getUrl()) // data's url
+                    .append(" - ").append(data.getDescription()).append("\n"); // data's description
+        }
+
+        if (dataContent.length() == lengthWithoutData) {
+            dataContent.append("\n").append("Well, no data yet");
+        }
+
+        return dataContent.toString();
     }
 
     private boolean isLabelInPath(String label) {
